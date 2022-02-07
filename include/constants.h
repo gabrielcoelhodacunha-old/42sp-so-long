32cdb5d099f1253a122ceb6f1d03b1eb8295f33a
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2022-02-07 16:15:30 -0300

    Defining component measures

--- include/constants.h
+++ include/constants.h
@@ -1,6 +1,7 @@
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 # define MAP_COMPONENTS "01CEP"
+# define XPM_EMPTY "../assets/empty.xpm"
 
 enum	e_components
 {
--- src/so_long.c
+++ src/so_long.c
@@ -4,12 +4,21 @@ void	so_long(void *mlx, t_matrix *map_description)
 {
 	t_coordinate	screen;
 	void	*window;
+	void	*empty;
+	t_coordinate	e;
 
 	(void) map_description;
 	mlx_get_screen_size(mlx, &screen.x, &screen.y);
-	window = mlx_new_window(mlx, screen.x / 2, screen.y / 2, "so_long");
+	screen.x = map_description->columns * 16;
+	screen.y = map_description->rows * 16;
+	window = mlx_new_window(mlx, screen.x, screen.y, "so_long");
+	//mlx_key_hook(window, handle_keyboard, mlx);
 	mlx_mouse_hook(window, handle_mouse, 0);
 	mlx_hook(window, DestroyNotify, StructureNotifyMask, handle_close, mlx);
+
+	empty = mlx_xpm_file_to_image(mlx, XPM_EMPTY, &e.x, &e.y);
+	(void) empty;
+
 	mlx_loop(mlx);
 	mlx_destroy_window(mlx, window);
 }
